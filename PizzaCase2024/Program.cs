b13f5fb852f649097994becb1e1018f177ad9bd9
Donut-Monkey <humanmonkey.minecraft@gmail.com>
2024-01-25 15:22:06 +0100

    Add project files.

---
+++ PizzaCase2024/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PizzaCase2024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
